bb1a54fc779ab0c64d61a2f3fe94b86fe8244784
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-10-21 22:08:35 +0100

    product schema unittest updated

--- scraper/tests/unittests/ProductSchemaTest.py
+++ scraper/tests/unittests/ProductSchemaTest.py
@@ -1,47 +1,46 @@
 import json
 import unittest
 
-from models.Nutrient import Nutrient
-from models.Nutrients import Nutrients
-from models.Price import Price
-from models.Product import Product
+
+from scraper.src.models.product.Nutrient import Nutrient
+from scraper.src.models.product.Nutrients import Nutrients
+from scraper.src.models.product.Price import Price
+from scraper.src.models.product.Product import Product
 
 
 class ProductSchemaTest(unittest.TestCase):
     def test_json_to_object_conversion(self):
+        product_data = json.loads(
+            '{"product_id": "sdW76wqygd", "product_name": "Test name", "price": {"currency": "\u00a3", '
+            '"retail_price": 2.34, "selling_price": 2.45}, "ingredients": "lorem lorem ipsum lorem lorem lorem", '
+            '"nutrients": {"main_nutrients": [{"name": "carbohydrate", "value": 4.0, "value_unit": "mg", "portion": '
+            '100.0, "portion_unit": "g"}], "minerals": [], "vitamins": []}, "customer_reviews": [], '
+            '"customer_rating": 4.8, "product_link": "http://...........", "plant_based": true, "meat_alternative": '
+            'true, "meat_taste": false, "meat_look": true, "counterpart_products": ["w3r@eqa", "asd2rqe3"]}')
 
-        product_data = json.dumps({
-            "product_id": "sdW76wqygd",
-            "product_name": "Test name",
-            "price": {
-                "currency": "£",
-                "retail_price": 2.34,
-                "selling_price": 2.45
-            },
-            "ingredients": "lorem lorem ipsum lorem lorem lorem",
-            "nutrients": {
-                "main_nutrients": [
-                    {
-                        "name": "carbohydrate",
-                        "value": 4,
-                        "value_unit": "mg",
-                        "portion": 100,
-                        "portion_unit": "g"
-                    }
-                ]
-            },
-            "customer_reviews": [],
-            "customer_rating": 4.8,
-            "product_link": "http://...........",
-            "plant_based": True,
-            "meat_alternative": True,
-            "meat_taste": False,
-            "meat_look": True,
-            "counterpart_products": ["w3r@eqa", "asd2rqe3"]
-        })
+        product_obj = Product(
+                product_id='sdW76wqygd',
+                product_name='Test name',
+                price=Price(currency='£', retail_price=2.34, selling_price=2.45),
+                ingredients='lorem lorem ipsum lorem lorem lorem',
+                nutrients=Nutrients(
+                    main_nutrients=[
+                        Nutrient(name="carbohydrate", value=4.0, value_unit="mg", portion=100.0, portion_unit="g")
+                    ],
+                ),
+                customer_rating=4.8,
+                product_link='http://...........',
+                plant_based=True,
+                meat_alternative=True,
+                meat_taste=False,
+                meat_look=True,
+                counterpart_products=['w3r@eqa', 'asd2rqe3']
+                )
 
-        self.assertEqual(Product.schema().loads(product_data),
-                         Product(product_id='sdW76wqygd', product_name='Test name', price=Price(currency='£', retail_price=2.34, selling_price=2.45), ingredients='lorem lorem ipsum lorem lorem lorem', nutrients=Nutrients(main_nutrients=[Nutrient(name='carbohydrate', value=4.0, value_unit='mg', portion=100.0, portion_unit='g')], minerals=[], vitamins=[]), customer_reviews=[], customer_rating=4.8, product_link='http://...........', plant_based=True, meat_alternative=True, meat_taste=False, meat_look=True, counterpart_products=['w3r@eqa', 'asd2rqe3']))  # add assertion here
+        self.assertEqual(
+            product_data,
+            product_obj.to_mongo().to_dict()
+        )
 
 
 if __name__ == '__main__':

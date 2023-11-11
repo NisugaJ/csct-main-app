09186528b63e35776815f09e147e9650575f2d3c
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-11 09:00:41 +0000

    added scrapy starter project

---
+++ scraper/scraper/src/components/supermarketscraper/supermarketscraper/spiders/scraper.py
@@ -0,0 +1,40 @@
+import scrapy
+from playwright.async_api import async_playwright
+
+class QuotesSpider(
+    scrapy.Spider
+    ):
+    name = "playwright"
+    start_urls = [
+        "data:,"
+        # "https://quotes.toscrape.com/tag/humor/",
+        # "https://groceries.asda.com/search/meat-poultry-fish/products?page=1"
+    ]
+
+    async def parse(self, response):
+        async with async_playwright() as pw:
+            browser = await pw.chromium.launch()
+            page = await browser.new_page()
+            await page.goto(
+                "https://groceries.asda.com/search/meat-poultry-fish/products?page=1"
+            )
+
+            open(
+                "meat-poultry-fish.html",
+                "w"
+                ).write(
+                await page.content()
+                )
+
+            # for quote in page.content().("div.co-product"):
+            #     yield {
+            #         "product_link": quote.xpath("div[2]/div[1]/h3/a/text()").get(),
+            #         # "text": quote.css("span.text::text").get(),
+            #     }
+            #
+            # next_page = response.css('li.next a::attr("href")').get()
+            # if next_page is not None:
+            #     yield response.follow(next_page, self.parse)
+
+            title = await page.title()
+            return {"title": title}

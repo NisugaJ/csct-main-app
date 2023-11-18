c8a6e062cb4350711d5ba20b162c265403b3b6ec
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-18 00:25:53 +0000

    spiders updates with yield to detailed and next pages

--- supermarketscraper/supermarketscraper/spiders/AsdaSpider.py
+++ supermarketscraper/supermarketscraper/spiders/AsdaSpider.py
@@ -3,33 +3,31 @@ from datetime import datetime
 from pprint import pprint
 
 import scrapy
+from scrapy.utils.display import pprint
 from scrapy.utils.reactor import install_reactor
 from scrapy_playwright.page import PageMethod
 
 
-class AsdaSpider(scrapy.Spider):
+class AsdaSpider(
+    scrapy.Spider
+    ):
     name = "asda-spider"
 
+    asda_base_url = "https://groceries.asda.com"
     allowed_domains = ["asda.com"]
-
-
+    url_with_page_attr = f"{asda_base_url}/search/meat-poultry-fish/products?page="
 
     def start_requests(self):
-        # GET request
         yield scrapy.Request(
-            "https://groceries.asda.com/search/meat-poultry-fish/products?page=1",
-            headers={
-               "User-Agent": "None"
-            },
+            f"{self.url_with_page_attr}1",
+            headers={"User-Agent": "None"},
             meta=dict(
                 playwright=True,
                 playwright_include_page=True,
-                playwright_page_methods=[
-                    PageMethod(
-                        "wait_for_selector",
-                        "div.co-product"
-                        ),
-                    # PageMethod(
+                playwright_page_methods=[PageMethod(
+                    "wait_for_selector",
+                    "div.co-product"
+                ),  # PageMethod(
                     #     "evaluate",
                     #     "window.scrollBy(0, document.body.scrollHeight)"
                     # ),
@@ -39,35 +37,91 @@ class AsdaSpider(scrapy.Spider):
                     # ),
                 ]
             ),
-            # errback=self.errback
         )
 
     async def parse(self, response):
-        open(
-            "response.html",
-            "w"
-            ).write(
-            response.text
-        )
 
         page = response.meta["playwright_page"]
-        await page.screenshot(path=f"/main-app/app/public/images/example-.png", full_page=True)
+        # await page.screenshot(path=f"/main-app/app/public/images/example-.png", full_page=True)
         await page.close()
 
-        for quote in response.css("div.co-product"):
-            yield {
-                "product_link": quote.xpath(
-                    "div[2]/div[1]/h3/a/text()"
-                    ).get(),
-                "text": quote.css(
-                    "span.text::text"
-                    ).get(),
-            }
+        for link in response.css("a.co-product__anchor::attr(href)"):
+            print(f"link: {link.get()}")
 
-        # next_page = response.css('li.next a::attr("href")').get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, self.parse)
+            yield scrapy.Request(
+                f"{self.asda_base_url}{link.get()}",
+                headers={"User-Agent": "None"},
+                meta=dict(
+                    playwright=True,
+                    playwright_include_page=True,
+                    playwright_page_methods=[PageMethod(
+                        "wait_for_selector",
+                        "div.product-detail-page__main-cntr"
+                    )]
+                ),
+                callback=self.product_parse
+            )
 
-    # async def errback(self, failure):
-    #     pprint(failure)
-    #     pprint(failure.request.meta)
+        # for request in self.search_page_request(f"{self.url_with_page_attr}{page_num}"):
+
+        maxPages = int(
+            response.css(
+                "div.co-pagination__max-page a::text"
+                ).get()
+            )
+        print(f"maxPages: {maxPages}")
+        # for page_num in range(2, maxPages):
+        #     for request in self.search_page_request(f"{self.url_with_page_attr}{page_num}"):
+        #         yield request
+
+    def product_parse(self, response):
+        yield {
+
+            # E.g. extracting from 'https://ui.assets-asda.com/dm/asdagroceries/5054781686002_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=YAbSt2&fmt=jpg&dpr=off&fit=constrain,1&wid=288&hei=288&qlt=5
+            # 0'
+            # "product_id": response.xpath("//div[@class = 'product-detail-page__zoomed-image-container']/div/picture/source/img/@src").get().split("dm/asdagroceries/")[1].split("_T1")[0],
+
+            "product_id": response.url.rsplit("/", 1)[1],
+            "product_name": response.css("div.pdp-main-details div[data-auto-id='titleRating'] h1::text").get(),
+
+            "price": {
+                "currency": response.css("div.pdp-main-details__price-container strong::text").get()[0],
+                "selling_price": response.css("div.pdp-main-details__price-container strong::text").get()[1:],
+                "weight": response.css("div.pdp-main-details__weight::text").get().strip(),
+            },
+            "ingredients": "",
+            "nutrients": "",
+            "customer_reviews": "",
+            "customer_rating": "",
+            "product_link": response.url,
+            "meat_alternative": "",
+            "meat_taste": "",
+            "meat_look": "",
+            "plant_based": False,
+            "dairy": False
+
+        }
+
+    def search_page_request(self, search_page_url):
+        yield scrapy.Request(
+            search_page_url,
+            headers={"User-Agent": "None"},
+            meta=dict(
+                playwright=True,
+                playwright_include_page=True,
+                playwright_page_methods=[PageMethod(
+                    "wait_for_selector",
+                    "div.co-product"
+                ),  # PageMethod(
+                    #     "evaluate",
+                    #     "window.scrollBy(0, document.body.scrollHeight)"
+                    # ),
+                    # PageMethod(
+                    #     "wait_for_selector",
+                    #     "div.co-product:nth-child(50)"
+                    # ),
+                ]
+            ),
+            callback=self.parse
+        )
+

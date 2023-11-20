d597014f25480c8f0378973d13621127b6149b6f
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-20 01:30:36 +0000

    spider updated to scrape most of the product fields

--- supermarketscraper/supermarketscraper/spiders/AsdaSpider.py
+++ supermarketscraper/supermarketscraper/spiders/AsdaSpider.py
@@ -1,11 +1,9 @@
-import logging
-from datetime import datetime
-from pprint import pprint
 
 import scrapy
-from scrapy.utils.display import pprint
-from scrapy.utils.reactor import install_reactor
 from scrapy_playwright.page import PageMethod
+
+from supermarketscraper.supermarketscraper.other_settings import DEFAULT_REQUEST_META
+
 
 
 class AsdaSpider(
@@ -19,11 +17,9 @@ class AsdaSpider(
 
     def start_requests(self):
         yield scrapy.Request(
-            f"{self.url_with_page_attr}1",
+            f"{self.asda_base_url}/search/meat-poultry-fish/products?page=1",
             headers={"User-Agent": "None"},
-            meta=dict(
-                playwright=True,
-                playwright_include_page=True,
+            meta=DEFAULT_REQUEST_META | dict(
                 playwright_page_methods=[PageMethod(
                     "wait_for_selector",
                     "div.co-product"
@@ -35,8 +31,9 @@ class AsdaSpider(
                     #     "wait_for_selector",
                     #     "div.co-product:nth-child(50)"
                     # ),
-                ]
+                ],
             ),
+            callback=self.parse
         )
 
     async def parse(self, response):
@@ -51,9 +48,7 @@ class AsdaSpider(
             yield scrapy.Request(
                 f"{self.asda_base_url}{link.get()}",
                 headers={"User-Agent": "None"},
-                meta=dict(
-                    playwright=True,
-                    playwright_include_page=True,
+                meta=DEFAULT_REQUEST_META | dict(
                     playwright_page_methods=[PageMethod(
                         "wait_for_selector",
                         "div.product-detail-page__main-cntr"
@@ -75,31 +70,38 @@ class AsdaSpider(
         #         yield request
 
     def product_parse(self, response):
+
+        nutrient_rows = response.xpath("//div[contains(@class, 'pdp-description-reviews__nutrition-row') and contains(@class, 'pdp-description-reviews__nutrition-row--details')]")
+        nutrients = []
+        for row in nutrient_rows:
+            nutrients.append(dict(
+                name_raw=row.xpath("div[1]/text()").get(),
+                portion_raw=row.xpath("div[2]/text()").get(),
+                # portion_unit=row.xpath("div[1]/text()").get().rsplit(" ", 1)[1] or row.xpath("div[2]/text()").get()[-1] or "",
+            ))
+
         yield {
 
             # E.g. extracting from 'https://ui.assets-asda.com/dm/asdagroceries/5054781686002_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=YAbSt2&fmt=jpg&dpr=off&fit=constrain,1&wid=288&hei=288&qlt=5
             # 0'
             # "product_id": response.xpath("//div[@class = 'product-detail-page__zoomed-image-container']/div/picture/source/img/@src").get().split("dm/asdagroceries/")[1].split("_T1")[0],
 
-            "product_id": response.url.rsplit("/", 1)[1],
-            "product_name": response.css("div.pdp-main-details div[data-auto-id='titleRating'] h1::text").get(),
+            "product_id": response.url.rsplit("/", 1)[1] or "",
+            "product_name": response.css("div.pdp-main-details div[data-auto-id='titleRating'] h1::text").get() or "",
 
             "price": {
-                "currency": response.css("div.pdp-main-details__price-container strong::text").get()[0],
-                "selling_price": response.css("div.pdp-main-details__price-container strong::text").get()[1:],
-                "weight": response.css("div.pdp-main-details__weight::text").get().strip(),
+                "selling_price": response.css("div.pdp-main-details__price-container strong::text").get()[1:] or "",
+                "weight": response.css("div.pdp-main-details__weight::text").get().strip() or "",
             },
-            "ingredients": "",
-            "nutrients": "",
-            "customer_reviews": "",
-            "customer_rating": "",
+            "ingredients": "".join(response.xpath("(//div[@class = 'pdp-description-reviews__product-details-content'])[5]/text()").getall()) or "",
+            "nutrients": nutrients or "",
+            "customer_rating": float(response.xpath("//div[contains(@class, 'rating-stars__stars') and contains(@class, 'rating-stars__stars--top')]/@style").get().rsplit(": ", 1)[1].rsplit("%", 1)[0])/20 or "",
             "product_link": response.url,
-            "meat_alternative": "",
-            "meat_taste": "",
-            "meat_look": "",
-            "plant_based": False,
-            "dairy": False
-
+            # "meat_alternative": False,
+            # "meat_taste": False,
+            # "meat_look": False,
+            # "plant_based": False,
+            # "dairy": False
         }
 
     def search_page_request(self, search_page_url):

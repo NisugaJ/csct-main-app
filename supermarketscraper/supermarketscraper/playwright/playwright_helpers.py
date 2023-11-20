4846eb1bcd100486ee5ef5fd6fb6a86342788a2a
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-20 01:28:57 +0000

    a new module added to maintain playwright functionalities with helper functions

---
+++ supermarketscraper/supermarketscraper/playwright/playwright_helpers.py
@@ -0,0 +1,21 @@
+#  Helper functions for playwright
+#  Copyright (c) 2023, Supermarketscraper
+
+"""
+Add page methods for playwright before initializing a page
+
+Args:
+    page (Page): The page object to initialize.
+    request (Request): The request object.
+
+Returns:
+    None
+"""
+import logging
+
+
+async def init_page(page, request):
+    logging.info("Initializing playwright page")
+    # Avoid loading images & videos
+    await page.route("**/*.{png,jpg,jpeg,.mp4}", lambda route: route.abort())
+

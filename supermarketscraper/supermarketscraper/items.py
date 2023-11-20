92655fdf59357aa5fd0cdee14a85e156d7702e68
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-20 01:27:16 +0000

    SupermarketscraperItem added for future usage with Scrapy Item Loaders

--- supermarketscraper/supermarketscraper/items.py
+++ supermarketscraper/supermarketscraper/items.py
@@ -9,4 +9,30 @@ import scrapy
 class SupermarketscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    product_id = scrapy.Field()
+    product_name = scrapy.Field()
+    # price = EmbeddedDocumentField(
+    #     PriceAndWeight
+    #     )
+    ingredients = scrapy.Field()
+    # nutrients = EmbeddedDocumentField(
+    #     Nutrients
+    #     )
+    # customer_reviews = ListField(
+    #     StringField(
+    #         null=False
+    #         )
+    #     )
+    customer_rating = scrapy.Field()
+    product_link = scrapy.Field()
+    meat_alternative = scrapy.Field()
+    meat_taste = scrapy.Field()
+    meat_look = scrapy.Field()
+    # counterpart_products = ListField(
+    #     ReferenceField(
+    #         'self'
+    #         )
+    #     )
+    plant_based = scrapy.Field()
+    dairy = scrapy.Field()

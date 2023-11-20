658519f2b067a4cee6df28461f52d38e8bd382e7
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-20 01:26:27 +0000

    nutrients model removed

--- app/models/product/Nutrients.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import ListField,  EmbeddedDocument, EmbeddedDocumentField
-
-from app.models.product.Nutrient import Nutrient
-
-
-class Nutrients(EmbeddedDocument):
-    main_nutrients = ListField(EmbeddedDocumentField(Nutrient))
-    minerals = ListField(EmbeddedDocumentField(Nutrient))
-    vitamins = ListField(EmbeddedDocumentField(Nutrient))

b080d0a53784260dfe9aa53ae8ffce70db18aa3d
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-20 01:34:32 +0000

    RUN_SUPERMARKET_SCRAPER env var added to enable/disable the scraper

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import os
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
 
 from app.db.connect import connect_to_db
 from app.helpers.initialize import initialize
-from supermarketscraper.scraper import Scraper
 
+##### API files #####
 # import api
 from app.api import index
+##### API files #####
+
+from supermarketscraper.SuperMarketScraper import SuperMarketScraper
 
 load_dotenv()
 
@@ -27,19 +30,6 @@ def read_root():
     """
     return {"app": "Scraper Application"}
 
-# @app.post("/start-scraper")
-# async def start_scraper(background_tasks: BackgroundTasks):
-#     # Run the supermarket scraper
-#     if os.getenv("RUN_SUPERMARKET_SCRAPER") == "True":
-#         scraper = Scraper()
-# 
-#         background_tasks.add_task(scraper.run_spiders)
-# 
-#         return {"message": "Notification sent in the background"}
-# 
-#     else:
-#         return {"message": "Scraper is disabled"}
-
 
 if __name__ == '__main__':
     # Connect to the database
@@ -47,6 +37,10 @@ if __name__ == '__main__':
 
     # Publish the schema
     initialize()
+
+    if os.getenv("RUN_SUPERMARKET_SCRAPER") == "True":
+        scraper = SuperMarketScraper()
+        scraper.run_spiders()
 
     # Run the app
     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=5842, log_level="info")

176ce3c2f633430e0dfdae8b23becf04ca690b67
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-11-24 11:35:08 +0000

    some utils

---
+++ utils/utils.py
@@ -0,0 +1,28 @@
+import decimal
+import secrets
+import os
+from pathlib import Path
+from types import NoneType
+
+
+def get_a_unique_image_name():
+    while True:
+        random_string = secrets.token_hex(8) + ".png"
+        file_path = os.path.join(f"{Path.cwd()}/app/public/images/", random_string)
+
+        if not os.path.exists(file_path):
+            break
+
+    return random_string
+
+def val(value, default_value="#not-found#"):
+    invalid_data_types = [
+        NoneType,
+    ]
+
+    if type(value) in invalid_data_types or value == "":
+        return default_value
+    elif type(value) in (int, float, type(decimal.Decimal)):
+        return -99
+    return value
+

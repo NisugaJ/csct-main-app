519def96f2e69d02bcfed165c72e737a3c3fd35c
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2023-12-03 22:46:53 +0000

    Price model changes

--- app/models/product/Price.py
+++ app/models/product/Price.py
@@ -2,5 +2,11 @@ from mongoengine import StringField, FloatField, EmbeddedDocument
 
 
 class PriceAndWeight(EmbeddedDocument):
-    selling_price = FloatField(required=True)
-    weight = StringField(required=True)
+    selling_price = FloatField()
+    weight = StringField()
+
+    raw_selling_price = StringField(
+        required=True
+    )
+    raw_weight = StringField( required=True)
+    raw_uom = StringField( required=True)

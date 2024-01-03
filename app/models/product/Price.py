055f3975bd73f653d86087c05a1d5f0c57b08fdf
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-03 00:24:35 +0000

    updating price model

--- app/models/product/Price.py
+++ app/models/product/Price.py
@@ -3,10 +3,15 @@ from mongoengine import StringField, FloatField, EmbeddedDocument
 
 class PriceAndWeight(EmbeddedDocument):
     selling_price = FloatField()
-    weight = StringField()
+    selling_size = StringField()
+    selling_size_type = StringField()
+
+    # 1 unit = 100g or 100ml
+    unit_price = FloatField()
+    unit_size_type = StringField()  # g,kg,ml,l
 
     raw_selling_price = StringField(
         required=True
     )
-    raw_weight = StringField( required=True)
-    raw_uom = StringField( required=True)
+    raw_weight = StringField(required=True)
+    raw_uom = StringField(required=True)

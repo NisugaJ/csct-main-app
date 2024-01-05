aa284466040a1d41b7da35f2cd7af6ea6053f136
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-05 05:20:42 +0000

    added a router to api

--- app/api/index.py
+++ app/api/index.py
@@ -1,9 +1,42 @@
 from pathlib import Path
 
+from fastapi import APIRouter
+from pydantic import BaseModel
 from starlette.staticfiles import StaticFiles
 
-from main import app
+from product_analyzer.components.query_model import QueryModel
+from product_analyzer.functions.query_pipeline import get_query_pipeline_from_model
 
-print(Path.cwd())
+app_router = APIRouter()
+
 # Public folder
-app.mount("/public", StaticFiles(directory=f"{Path.cwd()}/app/public"), name="static")
+app_router.mount("/public", StaticFiles(directory=f"{Path.cwd()}/app/public"), name="static")
+
+class Input(BaseModel):
+    input: str
+
+class Output(BaseModel):
+    output: str
+
+
+@app_router.get("/healthcheck")
+def healthcheck():
+    return {"status": "ok"}
+
+
+@app_router.post("/query")
+async def get_answers(query: Input):
+    query_text = query.q
+    if query_text == "" or query_text is None:
+        return "Please enter a query"
+
+    else:
+        model: QueryModel = get_query_pipeline_from_model()
+        model.connect_vector_store()
+
+        results = model.vector_store.similarity_search(
+            query=query,
+            k=5
+        )
+
+        return results
--- main.py
+++ main.py
@@ -1,23 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import multiprocessing
 import os
 
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.index import app_router
+
+
 import uvicorn
-from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from app.db.connect import connect_to_db
 from app.helpers.initialize import initialize
+from product_analyzer.utils.vector_store import prepare_vector_store
 from scraper_script import start_scraper
 
-#### Import API routes
-from app.api import index
-#### Import API routes
 
-load_dotenv()
-
-# Create the FastAPI app
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(app_router, prefix="/v1")
 
 @app.get("/")
 def read_root():
@@ -31,7 +41,7 @@ def read_root():
 
 
 if __name__ == '__main__':
-    # Connect to the database
+    # Connect to the Mongo database
     connect_to_db()
 
     # Publish the schema
@@ -41,6 +51,9 @@ if __name__ == '__main__':
         multiprocessing.set_start_method("spawn")
         multiprocessing.Process(target=start_scraper).start()
         print("Started supermarket scraper as a daemon process...")
+
+    if os.getenv("ADD_DATA_TO_VECTOR_STORE") == "True":
+        prepare_vector_store()
 
     # Run the app
     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=5842, log_level="info")

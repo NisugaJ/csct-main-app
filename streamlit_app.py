acb8fc8182bdfb5ce07d561466fbd066adca855e
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-06 05:33:54 +0000

    streamlit app as a separate script

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from st_aggrid import AgGrid
 import pandas as pd
 
-def start_streamlit_app():
-    st.set_page_config(page_title='AwesomeTable by @caiofaar', page_icon='📊', layout='wide')
-    st.title('AwesomeTable with Search')
+st.set_page_config(page_title='AwesomeTable by @caiofaar', page_icon='📊', layout='wide')
+st.title('AwesomeTable with Search')
 
-    st.sidebar.header('Planta')
+st.sidebar.header('Planta')
 
-    df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
-    AgGrid(df)
+df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+AgGrid(df)

09435916cde5893157b3406df8f3a82c1fad8ded
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-07 04:50:07 +0000

    chat ui added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 from st_aggrid import AgGrid
 import pandas as pd
+import numpy as np
 
-st.set_page_config(page_title='AwesomeTable by @caiofaar', page_icon='📊', layout='wide')
-st.title('AwesomeTable with Search')
+st.set_page_config(page_title="Home", page_icon='🌱', layout='wide', menu_items={
+    'Get Help': 'https://www.extremelycoolapp.com/help',
+    'Report a bug': "https://www.extremelycoolapp.com/bug",
+    'About': "# This is a header. This is an *extremely* cool app!"
+})
 
-st.sidebar.header('Planta')
+sidebar = st.sidebar
+sidebar.title(':green[Planta]', anchor=False)
+sidebar.write(':grey[Affordable plant based food better than meat]')
 
-df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
-AgGrid(df)
+
+# df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+# AgGrid(df)
+
